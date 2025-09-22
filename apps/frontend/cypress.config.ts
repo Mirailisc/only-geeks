fe06be3f75533448e0b084bac959cdf2e6b72c52
Mirailisc <mirailisclm@gmail.com>
2025-09-22 07:13:55 +0700

    cypress db seed & cleanup

--- apps/frontend/cypress.config.ts
+++ apps/frontend/cypress.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'cypress'
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor'
 import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild'
+import { PrismaClient } from '@package/prisma'
+import * as path from 'path'
+import * as dotenv from 'dotenv'
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+
+const prisma = new PrismaClient()
 
 export default defineConfig({
   e2e: {
@@ -20,6 +27,36 @@ export default defineConfig({
           plugins: [createEsbuildPlugin(config)],
         }),
       )
+
+      on('task', {
+        async 'db:seed'() {
+          await prisma.user.create({
+            data: {
+              email: 'admin@test.com',
+              firstName: 'Admin',
+              lastName: 'User',
+              isAdmin: true,
+            },
+          })
+          await prisma.user.create({
+            data: {
+              email: 'user@test.com',
+              firstName: 'User',
+              lastName: 'User',
+              isAdmin: false,
+            },
+          })
+          return null
+        },
+        async 'db:clean'() {
+          await prisma.user.deleteMany({
+            where: {
+              email: { in: ['admin@test.com', 'user@test.com'] },
+            },
+          })
+          return null
+        },
+      })
 
       return config
     },
--- apps/frontend/cypress/e2e/02.authentication.feature
+++ apps/frontend/cypress/e2e/02.authentication.feature
@@ -1,12 +1,17 @@
-Feature: Authentication 
+Feature: Authentication
 
   Scenario: View Profile
-    Given I am logged in as "test@example.com"
+    Given I am logged in as "user@test.com"
     Then I view my profile
-    Then I should see "test@example.com"
-  
+    Then I should see "user@test.com"
+
+  Scenario: Admin Login
+    Given I am logged in as "admin@test.com"
+    Then I view my profile
+    Then I should see "You are an admin"
+
   Scenario: Logout
-    Given I am logged in as "test@example.com"
+    Given I am logged in as "user@test.com"
     Then I view my profile
     Then I click logout button
     Then I should see "Hello World!"
