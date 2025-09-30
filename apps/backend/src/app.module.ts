50481e11615059a7c77378134d846b4c87f33549
Mirailisc <mirailisclm@gmail.com>
2025-09-30 23:31:51 +0700

    forgor controller

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 import { UserModule } from './user/user.module'
 import { AuthModule } from './auth/auth.module'
+import { AppController } from './app.controller'
 
 @Module({
   imports: [
@@ -24,5 +25,6 @@ import { AuthModule } from './auth/auth.module'
     UserModule,
     AuthModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
