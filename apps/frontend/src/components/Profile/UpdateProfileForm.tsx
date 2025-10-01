c4d5ca9fcb32ff3251684ee8347fa13dd6d356b1
mmawin99 <41543586+mmawin99@users.noreply.github.com>
2025-10-01 22:44:07 +0700

    feat: profile page UI, (Incomplete)

--- apps/frontend/src/components/Profile/UpdateProfileForm.tsx
+++ apps/frontend/src/components/Profile/UpdateProfileForm.tsx
@@ -10,6 +10,9 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import z from 'zod'
 import { Textarea } from '../ui/textarea'
 import { Button } from '../ui/button'
+// import { Switch } from '../ui/switch'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { Label } from '../ui/label'
 
 type Props = {
   profile: Profile
@@ -69,86 +72,107 @@ export default function UpdateProfileForm({ profile, setProfile }: Props) {
   }, [profile])
 
   return (
-    <div className="m-4 rounded-md border border-black/10 p-4">
+    <div className="rounded-md border border-black/10 p-4">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="firstName"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>First Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="First Name" {...field} data-cy="input-firstName" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <div className='flex-col items-center flex sm:flex-row gap-4 w-full'>
+            <div className='text-center space-y-4 w-max'>
+              <Label>Profile Image</Label>
+              <Avatar className='w-[180px] h-[180px] mb-4'>
+                <AvatarImage src={profile?.picture || undefined} alt={profile?.firstName || 'Avatar'} />
+                <AvatarFallback className='text-5xl'>
+                {profile.firstName[0].toUpperCase()}
+                {profile.lastName[0].toUpperCase()}
+              </AvatarFallback>
+              </Avatar>
+            </div>
+            <div className='flex flex-col space-y-4 w-full'>
+              <div className='flex flex-col md:flex-row gap-4 w-full'>
+                <FormField
+                  control={form.control}
+                  name="firstName"
+                  render={({ field }) => (
+                    <FormItem className='flex-1'>
+                      <FormLabel>First Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="First Name" className='w-full' {...field} data-cy="input-firstName" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="lastName"
+                  render={({ field }) => (
+                    <FormItem className='flex-1'>
+                      <FormLabel>Last Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Last Name" className='w-full' {...field} data-cy="input-lastName" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormItem className='flex-1'>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username" className='w-full' disabled />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              </div>
+              <FormField
+                control={form.control}
+                name="bio"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Bio</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Say something about yourself"
+                        maxLength={100}
+                        {...field}
+                        value={field.value ?? ''}
+                        onChange={(e) => field.onChange(e.target.value)}
+                        data-cy="input-bio"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col md:flex-row gap-4 w-full'>
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem className='flex-1'>
+                  <FormLabel>Location</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Location" {...field} data-cy="input-location" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="lastName"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Last Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Last Name" {...field} data-cy="input-lastName" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="bio"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Bio</FormLabel>
-                <FormControl>
-                  <Textarea
-                    placeholder="Say something about yourself"
-                    maxLength={100}
-                    {...field}
-                    value={field.value ?? ''}
-                    onChange={(e) => field.onChange(e.target.value)}
-                    data-cy="input-bio"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="location"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Location</FormLabel>
-                <FormControl>
-                  <Input placeholder="Location" {...field} data-cy="input-location" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="organization"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Organization</FormLabel>
-                <FormControl>
-                  <Input placeholder="Organization" {...field} data-cy="input-organization" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
+            <FormField
+              control={form.control}
+              name="organization"
+              render={({ field }) => (
+                <FormItem className='flex-1'>
+                  <FormLabel>Organization</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Organization" {...field} data-cy="input-organization" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
           <Button type="submit" className="w-full" disabled={updating} data-cy="submit">
             {updating ? 'Updating...' : 'Submit'}
           </Button>
--- apps/frontend/src/pages/profile/index.tsx
+++ apps/frontend/src/pages/profile/index.tsx
@@ -5,6 +5,7 @@ import { useQuery } from '@apollo/client/react'
 import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 import UpdateProfileForm from '@/components/Profile/UpdateProfileForm'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export interface Profile {
   id: string
@@ -37,11 +38,26 @@ export default function Profile() {
   if (loading || !user) return <div>Loading...</div>
 
   return (
-    <div>
+    <>
       <AuthNavbar />
-      <div>{JSON.stringify(profile, null, 2)}</div>
-      {user.isAdmin && <div>You are an admin</div>}
-      {profile && <UpdateProfileForm profile={profile} setProfile={setProfile} />}
-    </div>
+      <div className='container mx-auto'>
+        <div className="mb-8 mt-4">
+          <h1 className="text-3xl font-bold mb-2">My Profile</h1>
+          <p className="text-muted-foreground">Update your preference and profile info.</p>
+        </div>
+        {/* <div>{JSON.stringify(profile, null, 2)}</div> */}
+        {user.isAdmin && <div>You are an admin</div>}
+        <Tabs defaultValue="profile" className="space-y-4">
+          <TabsList>
+            <TabsTrigger value="profile">Profile</TabsTrigger>
+            <TabsTrigger value="appearance">Appearance</TabsTrigger>
+          </TabsList>
+          <TabsContent value="profile">
+            {profile && <UpdateProfileForm profile={profile} setProfile={setProfile} />}
+          </TabsContent>
+          <TabsContent value="appearance">Appearance</TabsContent>
+        </Tabs>
+      </div>
+    </>
   )
 }
