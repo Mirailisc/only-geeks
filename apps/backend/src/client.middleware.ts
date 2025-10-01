fbdf5d9348a8758ee5b65dc4adf7208cb161941b
Mirailisc <mirailisclm@gmail.com>
2025-10-01 21:31:38 +0700

    a

--- apps/backend/src/client.middleware.ts
+++ apps/backend/src/client.middleware.ts
@@ -5,10 +5,18 @@ import { join } from 'path'
 @Injectable()
 export class SpaFallbackMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    if (req.method !== 'GET') {
+      return next()
+    }
+
     if (req.path.startsWith('/auth') || req.path.startsWith('/graphql')) {
       return next()
     }
 
-    res.sendFile(join(__dirname, '..', 'public', 'index.html'))
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'), (err) => {
+      if (err) {
+        next(err)
+      }
+    })
   }
 }
