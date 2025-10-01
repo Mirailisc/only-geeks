04c751989c12b8f2fe2abfd65e40bde78f21a6c8
Mirailisc <mirailisclm@gmail.com>
2025-10-01 21:46:30 +0700

    a

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -24,7 +24,7 @@ import { ServeStaticModule } from '@nestjs/serve-static'
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-      renderPath: '/*',
+      renderPath: '/(.*)*',
       exclude: ['/auth*', '/graphql*'],
     }),
     HealthModule,
