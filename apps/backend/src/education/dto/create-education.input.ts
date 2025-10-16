11f4469b453423bcdbc0f2383e0e041ac6361184
smolguyonearth <poynitis@gmail.com>
2025-10-16 16:44:37 +0700

    fixed required field

--- apps/backend/src/education/dto/create-education.input.ts
+++ apps/backend/src/education/dto/create-education.input.ts
@@ -2,8 +2,8 @@ import { InputType, Field } from '@nestjs/graphql'
 
 @InputType()
 export class CreateEducationInput {
-  @Field({ nullable: true }) school?: string
-  @Field({ nullable: true }) degree?: string
+  @Field() school?: string
+  @Field() degree?: string
   @Field({ nullable: true }) fieldOfStudy?: string
   @Field({ nullable: true }) startDate?: Date
   @Field({ nullable: true }) endDate?: Date
