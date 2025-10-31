f404e2e9beca9b97861176caa69bae9b0b256508
mmawin99 <41543586+mmawin99@users.noreply.github.com>
2025-10-31 19:11:04 +0700

    fix: add authentication guard to feed query

--- apps/backend/src/feed/feed.resolver.ts
+++ apps/backend/src/feed/feed.resolver.ts
@@ -2,12 +2,15 @@ import { Args, Query, Resolver } from '@nestjs/graphql'
 import { FeedService } from './feed.service'
 import { FeedInput } from './dto/feed.input'
 import { FeedResponse } from './entities/feed.entity'
+import { UseGuards } from '@nestjs/common'
+import { GqlAuthGuard } from 'src/auth/guards/graphql-auth.guard'
 
 @Resolver()
 export class FeedResolver {
   constructor(private readonly feedService: FeedService) {}
 
   @Query(() => FeedResponse)
+  @UseGuards(GqlAuthGuard)
   async feed(@Args('input') input: FeedInput): Promise<FeedResponse> {
     return this.feedService.search(input)
   }
